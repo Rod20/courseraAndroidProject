f8edaf5be1f9a2edd9336d927c2bca4a5f808c8f
Rod20 <nimrod2560@gmail.com>
2020-07-04 17:58:01 -0400

    Add FloatingActionButton

--- app/src/main/java/com/courseraproject/app/MainActivity.java
+++ app/src/main/java/com/courseraproject/app/MainActivity.java
@@ -2,6 +2,9 @@ package com.courseraproject.app;
 
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +12,16 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        agregarFab();
+    }
+
+    public void agregarFab(){
+        FloatingActionButton miFav = findViewById(R.id.fabMyFab);
+        miFav.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
     }
 }
